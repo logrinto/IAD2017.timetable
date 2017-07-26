f4838cfcab161792ef8233caf1a167cbb622aa33
signalwerk <sh@signalwerk.ch>
2017-07-26 02:41:24 +0200

    MOD: new rsa keys

--- travis/gh-pages.sh
+++ travis/gh-pages.sh
@@ -38,14 +38,12 @@ DEPLOY_DIR="public"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    doCompile
     exit 0
 fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
 echo "----- repo"
-echo ${REPO}
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
@@ -62,17 +60,12 @@ rm -rf $DEPLOY_DIR/**/* || exit 0
 
 # Run our compile script
 echo "-- build"
-ls -las
 sh ./travis/build.sh
-ls -las
 
 # Now let's go have some fun with the cloned repo
 cd $DEPLOY_DIR
-ls -las
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-git diff
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then
