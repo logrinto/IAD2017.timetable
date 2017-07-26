333d9e885e4419b4a01819849c7de3887f40d0e2
signalwerk <sh@signalwerk.ch>
2017-07-26 15:44:22 +0200

    ADD: experimental ICS support

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let plan = new Semesterplan({
   data: './src/data/data.yml',
   template: './src/template/main.hbs',
   dest: './public/index.html',
+  ics: './public/IAD.ics',
 });
 
 plan.process()
+plan.ics()
--- src/semesterplan.js
+++ src/semesterplan.js
@@ -2,7 +2,11 @@ import handlebars from 'handlebars';
 import fs from 'fs';
 import yaml from 'js-yaml';
 import _ from 'lodash';
+import { Component, Property } from 'immutable-ics'
+import moment from 'moment';
+import uuid from 'uuid';
 
+const now = new moment();
 var loadBuiltinHelpers = require('./loadBuiltinHelpers');
 var loadPartials = require('./loadPartials');
 
@@ -30,6 +34,7 @@ class Semesterplan {
     this.path_data = data.data || '';
     this.path_template = data.template || '';
     this.path_dest = data.dest || '';
+    this.path_ics = data.ics || '';
   }
 
   loader() {
@@ -94,9 +99,159 @@ class Semesterplan {
     data.lessions = _.sortKeysBy(data.lessions, (item) => { return item.title; });
   }
 
-  process() {
 
+  icsEvent(data) {
+    // console.log(JSON.stringify(data, null, '  '))
+
+    var title = data.comment || '';
+
+    if (data.resolve.lession && data.resolve.lession.title) {
+      title = data.resolve.lession.title;
+    }
+
+    var event = new Component({
+            name: 'VEVENT',
+            // components: [
+            //   new Component({
+            //     name: 'VALARM',
+            //     properties: [
+            //       new Property({ name: 'ACTION', value: 'DISPLAY' }),
+            //       new Property({ name: 'TRIGGER', value: '-PT12H' }),
+            //       new Property({ name: 'DESCRIPTION', value: 'Event reminder' })
+            //     ]
+            //   })
+            // ],
+            properties: [
+              new Property({ name: 'UID', value: uuid.v1() }),
+              new Property({
+                name: 'DTSTAMP',
+                value: now,
+                parameters: {
+                  VALUE: 'DATE-TIME',
+                  TZID: 'Europe/Zurich',
+                }
+              }),
+              new Property({
+                name: 'SUMMARY',
+                value: title,
+              }),
+              new Property({
+                name: 'DTSTART',
+                value: data.resolve.dates.start,
+                parameters: {
+                  VALUE: 'DATE-TIME',
+                  TZID: 'Europe/Zurich',
+                }
+              }),
+              new Property({
+                name: 'DTEND',
+                value: data.resolve.dates.end,
+                parameters: {
+                  VALUE: 'DATE-TIME',
+                  TZID: 'Europe/Zurich',
+                }
+              }),
+              new Property({
+                name: 'DESCRIPTION',
+                value: 'Lehrer: ' + (data.resolve.teacher || 'noch offen'),
+              }),
+              // new Property({
+              //   name: 'ATTENDEE',
+              //   parameters: {
+              //     CN: 'Sample Company',
+              //     RSVP: 'FALSE:foo@example.com'
+              //   }
+              // })
+            ]
+          });
+          return event;
+  }
+
+  ics() {
+    var events = [];
+    let data = this.data;
+
+    _.forEach(data.semester.planing, (value, key) => {
+
+      if (value.FR) {
+        events.push(this.icsEvent(value.FR.morning))
+        events.push(this.icsEvent(value.FR.afternoon))
+      }
+      if (value.SA) {
+        events.push(this.icsEvent(value.SA.morning))
+        events.push(this.icsEvent(value.SA.afternoon))
+      }
+    });
+
+    const calendar = new Component({
+      name: 'VCALENDAR',
+      components: events,
+      properties: [
+        new Property({ name: 'VERSION', value: 2 }),
+        new Property({ name: 'PRODID', value: 'logrinto' })
+      ]
+    })
+
+    let out = calendar.toString();
+    fs.writeFileSync(this.path_ics, out);
+  }
+
+
+  dataResolve() {
+    let data = this.data;
+
+    _.forEach(data.semester.planing, (value, key) => {
+      // value.resolve = {
+      //   dates: {
+      //     fr: new moment(value.KW, '[KW]ww-YYYY').add(5, 'd'),
+      //     sa: new moment(value.KW, '[KW]ww-YYYY').add(6, 'd')
+      //   }
+      // }
+
+      if (value.FR) {
+        value.FR.morning.resolve = {
+          dates: {
+            start: new moment(value.KW, '[KW]ww-YYYY').add(5, 'd').hour(8).minute(5),
+            end: new moment(value.KW, '[KW]ww-YYYY').add(5, 'd').hour(11).minute(40),
+          },
+          teacher: data.teachers[value.FR.morning.teacher] || '',
+          lession: data.lessions[value.FR.morning.lession] || {},
+        }
+        value.FR.afternoon.resolve = {
+          dates: {
+            start: new moment(value.KW, '[KW]ww-YYYY').add(5, 'd').hour(13).minute(15),
+            end: new moment(value.KW, '[KW]ww-YYYY').add(5, 'd').hour(16).minute(45),
+          },
+          teacher: data.teachers[value.FR.afternoon.teacher] || '',
+          lession: data.lessions[value.FR.afternoon.lession] || {},
+        }
+      }
+
+      if (value.SA) {
+        value.SA.morning.resolve = {
+          dates: {
+            start: new moment(value.KW, '[KW]ww-YYYY').add(6, 'd').hour(8).minute(5),
+            end: new moment(value.KW, '[KW]ww-YYYY').add(6, 'd').hour(11).minute(40),
+          },
+          teacher: data.teachers[value.SA.morning.teacher],
+          lession: data.lessions[value.SA.morning.lession] || {},
+        }
+        value.SA.afternoon.resolve = {
+          dates: {
+            start: new moment(value.KW, '[KW]ww-YYYY').add(6, 'd').hour(13).minute(15),
+            end: new moment(value.KW, '[KW]ww-YYYY').add(6, 'd').hour(16).minute(45),
+          },
+          teacher: data.teachers[value.SA.afternoon.teacher],
+          lession: data.lessions[value.SA.afternoon.lession] || {},
+        }
+      }
+
+    });
+  }
+
+  process() {
     this.loader();
+    this.dataResolve();
     this.calcTotal();
 
     let template = handlebars.compile(this.template);
