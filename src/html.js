3700e857bfcecc43d9eb17700cc8c044bfa036a1
signalwerk <sh@signalwerk.ch>
2017-10-19 13:40:31 +0200

    MOD: adjust width

--- src/html.js
+++ src/html.js
@@ -27,7 +27,7 @@ module.exports = class HTML extends React.Component {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta
             name="viewport"
-            content="width=500, initial-scale=1"
+            content="width=800"
           />
           {/*<meta
             name="viewport"
