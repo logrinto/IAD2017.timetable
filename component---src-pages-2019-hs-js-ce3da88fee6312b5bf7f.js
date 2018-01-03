ea19ea2637f03ea9303f8f46b52727bad833a824
Travis CI <sh@signalwerk.ch>
2018-01-03 15:08:53 +0000

    Deploy to GitHub Pages: b8990836424da817e2121340ec0a5c4f39343bae

--- component---src-pages-2019-hs-js-ce3da88fee6312b5bf7f.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([37401820312762],{192:function(e,t,a){"use strict";function s(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0,t.query=void 0;var r=a(2),d=s(r),n=a(21),u=(s(n),a(22)),l=s(u),o=function(e){var t=e.data,a=t.allIad2017Yaml.edges[4].node.data;return d.default.createElement(l.default,{semester:a.semester,data:a.date,teachers:a.teachers,lessions:a.lessions})};t.query="** extracted graphql fragment **";t.default=o}});
-//# sourceMappingURL=component---src-pages-2019-hs-js-ce3da88fee6312b5bf7f.js.map
