4972deec8d2693bb443d709580803225e81310bc
signalwerk <sh@signalwerk.ch>
2018-02-19 02:16:00 +0100

    MOD: adjust styling

--- src/html.js
+++ src/html.js
@@ -1,34 +1,31 @@
-import React from "react"
+import React from "react";
 
-let stylesStr
+let stylesStr;
 if (process.env.NODE_ENV === `production`) {
   try {
-    stylesStr = require(`!raw-loader!../public/styles.css`)
+    stylesStr = require(`!raw-loader!../public/styles.css`);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 module.exports = class HTML extends React.Component {
   render() {
-    let css
+    let css;
     if (process.env.NODE_ENV === `production`) {
       css = (
         <style
           id="gatsby-inlined-css"
           dangerouslySetInnerHTML={{ __html: stylesStr }}
         />
-      )
+      );
     }
     return (
       <html>
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=640"
-          />
+          <meta name="viewport" content="width=640" />
           {/*<meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
@@ -46,6 +43,6 @@ module.exports = class HTML extends React.Component {
           {this.props.postBodyComponents}
         </body>
       </html>
-    )
+    );
   }
-}
+};
